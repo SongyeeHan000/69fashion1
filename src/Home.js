ea04c67f5af43d724ddc60ad137367e88b9f52b3
SongyeeHan000 <hansongyee0304@gmail.com>
2022-11-30 23:07:38 -0500

    started on basic components

---
+++ src/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src="/dressShop1.png" className="d-block w-100" alt="dressShop1"/>
+        </div>
+        <div className="carousel-item">
+          <img src="/dressShop2.png" className="d-block w-100" alt="dressShop2"/>
+        </div>
+        <div className="carousel-item">
+          <img src="/dressShop3.png" className="d-block w-100" alt="dressShop3"/>
+        </div>
+      </div>
+      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+  )
+}
