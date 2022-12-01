4bccc0cde4c6d3c507ffe09d3b768b94d8d37f65
SongyeeHan000 <hansongyee0304@gmail.com>
2022-11-30 23:09:25 -0500

    added navbar bootstrap and cdn to html

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Suspense} from 'react'
+import {BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Navbar from "./Navigation/Navbar.js";
+
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+import FAQ from './FAQ';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route exact path="/about" element={<About/>} />
+          <Route exact path="/contact" element={<Contact/>} />
+          <Route exact path="/faq" element={<FAQ/>} />
+        </Routes>
+      </Suspense>
+    </Router>
   );
 }
 
