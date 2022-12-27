75a90c60d3caacc2c943fc38a0b5372ff8057d6d
Songyee Han <hansongyee0304@gmail.com>
2022-12-26 22:33:21 -0500

    edited the FAQ page with accordion drop downs and HashLink

--- src/FAQ.js
+++ src/FAQ.js
@@ -1,59 +1,77 @@
 import React from 'react'
+import Accordion from 'react-bootstrap/Accordion'
+import { HashLink } from 'react-router-hash-link';
+
 // use scrollspy to get one question to the dropdown questionbox
 export default function FAQ() {
+
   return (
-  <div>
-    <h1 style={{textAlign:"center"}}>FAQ</h1>
-    <div style={{margin:'2rem 5rem'}}>
-      <div id="accordion" role="tablist">
-        <div class="card">
-          <div class="card-header" role="tab" id="headingOne">
-            <h5 class="mb-0">
-              <a data-toggle="collapse" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                Collapsible Group Item #1
-              </a>
-            </h5>
-      </div>
-      <div id="collapseOne" class="collapse show" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion">
-            <div class="card-body">
-              ...............................................................................#1
-            </div>
-      </div>
-      </div>
-      <div class="card">
-          <div class="card-header" role="tab" id="headingTwo">
-            <h5 class="mb-0">
-              <a class="collapsed" data-toggle="collapse" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                Collapsible Group Item #2
-              </a>
-            </h5>
-      </div>
-      <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo" data-parent="#accordion">
-            <div class="card-body">
-              ...............................................................................#2
-            </div>
-      </div>
-      </div>
-      <div class="card">
-          <div class="card-header" role="tab" id="headingThree">
-            <h5 class="mb-0">
-              <a class="collapsed" data-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                Collapsible Group Item #3
-              </a>
-            </h5>
-      </div>
-      <div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#accordion">
-            <div class="card-body">
-            ...............................................................................#3
-            </div>
-      </div>
-      </div>
-      </div>
+    <Accordion>
+      {/* <ul>
+        <li><HashLink smooth to="#returnAndExchange">Return/Exchange Policies</HashLink></li>
+        <li><HashLink smooth to="#dressAndSizes">Dresses & Sizes</HashLink></li>
+        <li><HashLink smooth to="#storeInfo">Store Information</HashLink></li>
+      </ul>
+       */}
+      <h1 id='returnAndExchange'>Return/Exchange Policies</h1>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Do you ship dresses?</Accordion.Header>
+        <Accordion.Body>
+          No, we do not. We only allow in-person purchases, so come to our store soon!
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Can I return or exchange the dress after it has been purchased, but never worn?</Accordion.Header>
+        <Accordion.Body>
+          Our policy states that every purchase is final sale. We do not allow returns or exchanges, so please try on our dresses and examine the dress carefully before purchasing our dress{'('}es{')'}. 
+          We do not allow returns or exchange due to delicate embellishments on our dresses. 
+          </Accordion.Body>
+      </Accordion.Item>
+      
+      <h1 id='dressAndSizes'>Dresses & Sizes</h1>
+      <Accordion.Item eventKey="">
+        <Accordion.Header>What kind of dresses do you sell?</Accordion.Header>
+        <Accordion.Body>
+          We sell dresses for special occasions for Quinceanera, Prom, Sweet 16, Weddings, and Cocktails. 
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="">
+        <Accordion.Header>Do you guys sell/rent tuxedo?</Accordion.Header>
+        <Accordion.Body>
+          Sorry, we do not. We only sell women's dresses
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="">
+        <Accordion.Header>What is your range in sizes?</Accordion.Header>
+        <Accordion.Body>
+          We have sizes from XS to 3XL. 
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="">
+        <Accordion.Header>Do you alter dresses?</Accordion.Header>
+        <Accordion.Body>
+          No, we do not. 
+        </Accordion.Body>
+      </Accordion.Item>
+  
 
+      <h1 id='storeInfo'>Store Information</h1>
+      <Accordion.Item eventKey="">
+        <Accordion.Header>Where is the store located?</Accordion.Header>
+        <Accordion.Body>
+          Store is located in Bronx, NY.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="">
+        <Accordion.Header>Are you by appointment only?</Accordion.Header>
+        <Accordion.Body>
+         No, we are not. Feel free to come in anytime between 12pm - 6:30pm everyday!
+        </Accordion.Body>
+      </Accordion.Item>
+      
 
-    </div>
-    
-  </div>
+    </Accordion>
+  
     
   )
 }
