1570198fe614c3d3ab11a735811600991121b301
Songyee Han <hansongyee0304@gmail.com>
2023-01-18 01:58:38 -0500

    FAQ question updates

--- src/FAQ.js
+++ src/FAQ.js
@@ -15,7 +15,7 @@ export default function FAQ() {
       </ul>
        */}
        <div style={{margin:'3rem', border: 'solid black 1px', padding:'2rem'}}>
-       <h1 id='returnAndExchange' className='accordionTitle' style={{marginTop:'0px'}}>Return/Exchange Policies</h1>
+       <h1 id='returnAndExchange' className='accordionTitle' style={{marginTop:'0px'}}>Return/Exchange/Layaway Policies</h1>
       <Accordion.Item eventKey="0">
         <Accordion.Header>Do you ship dresses?</Accordion.Header>
         <Accordion.Body>
@@ -23,48 +23,67 @@ export default function FAQ() {
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="1">
-        <Accordion.Header>Can I return or exchange the dress after it has been purchased, but never worn?</Accordion.Header>
+        <Accordion.Header>Can I return or exchange the dress after it has been purchased, even if it has never been worn?</Accordion.Header>
         <Accordion.Body>
-          Our policy states that every purchase is final sale. We do not allow returns or exchanges, so please try on our dresses and examine the dress carefully before purchasing our dress{'('}es{')'}. 
+          Our policy states that every purchase is <span style={{fontWeight:'bold',textDecoration:'underline'}}>FINAL SALE</span>. We <span style={{fontWeight:'bold',textDecoration:'underline'}}>DO NOT ALLOW</span> returns or exchanges, so please try on our dresses and examine the dress carefully before purchasing our dresses. 
           We do not allow returns or exchange due to delicate embellishments on our dresses. 
           </Accordion.Body>
       </Accordion.Item>
-      
-      <h1 id='dressAndSizes' className='accordionTitle'>Dresses & Sizes</h1>
       <Accordion.Item eventKey="2">
+        <Accordion.Header>Can I do a layaway payment?</Accordion.Header>
+        <Accordion.Body>
+          No, we do not do layaway payments anymore.
+        </Accordion.Body>
+      </Accordion.Item>
+      
+      <h1 id='dressAndSizes' className='accordionTitle'>Dresses/Tuxedos & Sizes</h1>
+      <Accordion.Item eventKey="3">
         <Accordion.Header>What kind of dresses do you sell?</Accordion.Header>
         <Accordion.Body>
           We sell dresses for special occasions for Quinceanera, Prom, Sweet 16, Weddings, and Cocktails. 
         </Accordion.Body>
       </Accordion.Item>
-      <Accordion.Item eventKey="3">
-        <Accordion.Header>Do you guys sell/rent tuxedo?</Accordion.Header>
+      <Accordion.Item eventKey="7">
+        <Accordion.Header>Can I rent a dress?</Accordion.Header>
         <Accordion.Body>
-          Sorry, we do not. We only sell women's dresses
+          We do not rent dresses to customers. Customers can only buy dresses from our store.
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="4">
-        <Accordion.Header>What is your range in sizes?</Accordion.Header>
+        <Accordion.Header>Do you sell/rent tuxedo?</Accordion.Header>
+        <Accordion.Body>
+          Sorry for the inconvenience, but we do not sell or rent tuxedo. 
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="5">
+        <Accordion.Header>What is your size range?</Accordion.Header>
         <Accordion.Body>
           We have sizes from XS to 3XL. 
         </Accordion.Body>
       </Accordion.Item>
-      <Accordion.Item eventKey="5">
-        <Accordion.Header>Do you alter dresses?</Accordion.Header>
+      <Accordion.Item eventKey="6">
+        <Accordion.Header>Do you have Alterations & Tailoring Services?</Accordion.Header>
         <Accordion.Body>
-          No, we do not. 
+          No, we do not alter dresses. However, we will find the best fitting dress for you!
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="7">
+        <Accordion.Header>If 6&9 does not have my size, can I make a "special" order?</Accordion.Header>
+        <Accordion.Body>
+          No,we cannot make "special" orders. We only sell dresses that are available at the store.
         </Accordion.Body>
       </Accordion.Item>
   
 
       <h1 id='storeInfo' className='accordionTitle'>Store Information</h1>
-      <Accordion.Item eventKey="6">
+      <Accordion.Item eventKey="8">
         <Accordion.Header>Where is the store located?</Accordion.Header>
         <Accordion.Body>
-          Store is located in Bronx, NY.
+          Our store is located in Bronx, NY. Customers from Manhattan can take the uptown 4 or D train to "Fordham Road" to reach our store! 
         </Accordion.Body>
       </Accordion.Item>
-      <Accordion.Item eventKey="7">
+
+      <Accordion.Item eventKey="9">
         <Accordion.Header>Are you by appointment only?</Accordion.Header>
         <Accordion.Body>
          No, we are not. Feel free to come in anytime between 12pm - 6:30pm everyday!
